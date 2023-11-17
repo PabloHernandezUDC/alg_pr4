0750735abc862de4b73520cac99cdce4bda76549
PabloHernandezUDC <125274113+PabloHernandezUDC@users.noreply.github.com>
2023-11-17 10:52:03 +0100

    puede que ya esté??

--- source.py
+++ source.py
@@ -6,9 +6,21 @@ def matrizAleatoria(n):
     return (np.tril(m, -1) + np.tril(m, -1).T)
 
 def minDistintoDeCero(l):
-    res = min(l)
-    if min 
-    pass
+    # debemos devolver el índice del mínimo distinto de cero
+    zeroIndex = 0
+    for i in range(len(l) - 1):
+        if l[i] == 0:
+            zeroIndex = i
+            break # asumimos que solo hay uno y nos quedamos con el primero
+    l = np.delete(l, zeroIndex)
+    minIndex = np.where(l == min(l))[0][0]
+    # hay que coger los índices así porque si no es un objeto array
+    
+    if minIndex >= zeroIndex:
+        minIndex += 1
+        # si estaba a la derecha del cero, hay que compensar y sumarle 1
+    
+    return minIndex
 
 def dijkstra(matriz):
     print(matriz)
@@ -27,11 +39,10 @@ def dijkstra(matriz):
         for i in range(n-1):
             # v es el nodo que tiene la menor distancia a m Y que no esté visitado
             fila = distancias[m]
-            v = 00000000000000
-            print(v)
-            
-        print('----')
+            v = minDistintoDeCero(fila) # es el índice, no el valor
+            noVisitados = np.delete(noVisitados, v)
+            for w in noVisitados:
+                if distancias[m][w] > distancias[m][v] + matriz[v][w]:
+                    distancias[m][w] = distancias[m][v] + matriz[v][w]
 
-#dijkstra(matrizAleatoria(5))
-
-lista = [0,1,2,3,4]
+dijkstra(matrizAleatoria(5))
